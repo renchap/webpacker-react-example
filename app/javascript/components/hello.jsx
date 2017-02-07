ff782d76918e5e70703e370073872f9a553c0dc4
Miklos Fazekas <mfazekas@szemafor.com>
2017-02-07 13:10:42 +0100

    Added state to Hello component

--- app/javascript/components/hello.jsx
+++ app/javascript/components/hello.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
+
+  incrementCount() {
+    this.setState({count: this.state.count+1})
+  }
+
   render() {
     return <div>
-             Hello, I am {this.props.name}!
+             Hello, I am {this.props.name} my sate is: {this.state.count}! <button onClick={() => this.incrementCount()}>increment</button>
            </div>
   }
 }
